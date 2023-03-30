13195def65b81e2917caa2dde89ec88f0d820556
frva0001 <frva0001@gmail.com>
2023-03-30 13:32:15 +0200

    done

--- script.js
+++ script.js
@@ -27,27 +27,23 @@ function showCharacter(character) {
    document.querySelector("#character article:last-child").addEventListener("click", characterClicked);
 
    function characterClicked() {
-      showDialogue();
+      document.querySelector("#dialog-name").textContent = character.name;
+      document.querySelector("#dialog-nickname").textContent = character.nickname;
+      document.querySelector("#dialog-image").innerHTML = `<p><img src= ${character.image}/></p>`;
+      document.querySelector("#dialog-occupation").textContent = character.occupation;
+      document.querySelector("#dialog-age").textContent = character.age;
+      document.querySelector("#dialog-voicedBy").textContent = character.voicedBy;
+      document.querySelector("#dialog-gender").textContent = character.gender;
+      document.querySelector("#dialog-religion").textContent = character.religion;
+      document.querySelector("#dialog-catchPhrase").textContent = character.catchPhrase;
+      document.querySelector("#dialog-hairColor").textContent = character.hairColor;
+      document.querySelector("#dialog-schoolGrade").textContent = character.schoolGrade;
+      document.querySelector("#dialog-episodes").textContent = character.episodes;
+      document.querySelector("#dialog-appearances").textContent = character.appearances;
+      document.querySelector("#dialog-firstAppearance").textContent = character.firstAppearance;
+
+      document.querySelector("dialog").showModal();
    }
-}
-
-function showDialogue() {
-   document.querySelector("#dialog-name").textContent = character.name;
-   document.querySelector("#dialog-nickname").textContent = character.nickname;
-   document.querySelector("#dialog-image").innerHTML = `<p><img src= ${character.image}/></p>`;
-   document.querySelector("#dialog-occupation").textContent = character.occupation;
-   document.querySelector("#dialog-age").textContent = character.age;
-   document.querySelector("#dialog-voicedBy").textContent = character.voicedBy;
-   document.querySelector("#dialog-gender").textContent = character.gender;
-   document.querySelector("#dialog-religion").textContent = character.religion;
-   document.querySelector("#dialog-catchPhrase").textContent = character.catchPhrase;
-   document.querySelector("#dialog-hairColor").textContent = character.hairColor;
-   document.querySelector("#dialog-schoolGrade").textContent = character.schoolGrade;
-   document.querySelector("#dialog-episodes").textContent = character.episodes;
-   document.querySelector("#dialog-appearances").textContent = character.appearances;
-   document.querySelector("#dialog-firstAppearance").textContent = character.firstAppearance;
-
-   document.querySelector("dialog").showModal();
 }
 
 function showAllCharacters(characters) {
